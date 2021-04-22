003be4808993d4217106bf3f62ca93eafa1d15c7
jjader03 <jjader03@gmail.com>
2021-04-22 00:12:28 -0300

    add mapa

--- src/screens/map.js
+++ src/screens/map.js
@@ -1,10 +1,28 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import * as React from 'react';
+import MapView from 'react-native-maps';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import { mapStyle } from '../components/dados/map'
 
-export default function map() {
+export default function App() {
   return (
-    <View>
-      <Text>Map</Text>
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        customMapStyle={mapStyle}
+      />
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+});
